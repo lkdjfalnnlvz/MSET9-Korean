10ceab0f8799289fdcab10fef8036a4ebfbad144
Danny Tsai <danny@saru.moe>
2024-08-14 14:17:43 +0800

    Update MSET9-Windows.bat
    Try to check common python launcher path to avoid missing PATH issue

--- MSET9_installer_script/MSET9-Windows.bat
+++ MSET9_installer_script/MSET9-Windows.bat
@@ -1,12 +1,24 @@
 @echo off
 chcp 65001 > nul
-py -V > nul
-if %errorlevel% NEQ 0 (
-    echo Python 3 is not installed.
-    echo Please install Python 3 and try again.
-    echo https://www.python.org/downloads/
-    echo.
-    pause
-    exit
+
+set PY1="py"
+set PY2="%WINDIR%\py"
+set PY3="%LOCALAPPDATA%\Programs\Python\Launcher\py"
+
+setlocal enableDelayedExpansion
+for /l %%x in (1, 1, 3) do (
+	set PY=!PY%%x!
+	!PY! -V > nul 2> nul
+	if !errorlevel! EQU 0 goto found
 )
-py -3 mset9.py
+endlocal
+
+echo Python 3 is not installed.
+echo Please install Python 3 and try again.
+echo https://www.python.org/downloads/
+echo.
+pause
+exit
+
+:found
+%PY% -3 mset9.py
